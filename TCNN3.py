1f5d36d480061d700950260166c3861c88dab9bb
exaltation <jorillo311@gmail.com>
2017-04-27 18:21:01 +0500

    +

--- TCNN3.py
+++ TCNN3.py
@@ -10,37 +10,37 @@ def create_model(num_classes):
     # first Conv2D+Relu
     x = Conv2D(96,
     input_shape=(227, 227, 3),
-    kernel_size=11,
-    strides=4,
+    kernel_size=(11, 11),
+    strides=(4, 4),
     activation="relu",
     kernel_initializer=TruncatedNormal(stddev=0.01))
 
     # first Pooling
-    x = MaxPooling2D(pool_size=3, strides=2)(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x)
 
     # first Local response normalization
     x = LRN2D()(x)
 
     # second Conv2D+Relu
     x = Conv2D(256,
-    kernel_size=5,
+    kernel_size=(5, 5),
     activation="relu",
     kernel_initializer=TruncatedNormal(stddev=0.01))(x)
 
     # second Pooling
-    x = MaxPooling2D(pool_size=3, strides=2)(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x)
 
     # second Local response normalization
     x = LRN2D()(x)
 
     # third Conv2D+Relu
     x = Conv2D(384,
-    kernel_size=5,
+    kernel_size=(5, 5),
     activation="relu",
     kernel_initializer=TruncatedNormal(stddev=0.01))(x)
 
     # Energy layer
-    x = AveragePooling2D(pool_size=13)(x)
+    x = AveragePooling2D(pool_size=(13, 13))(x)
     x = Flatten()(x)
 
     # Fully connected layers
