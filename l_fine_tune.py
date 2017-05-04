c7241f0eb923e1e206a7e48791a8be5a9bd17bd8
Jorillo <jorillo311@gmail.com>
2017-05-04 19:11:41 +0500

    +

--- l_fine_tune.py
+++ l_fine_tune.py
@@ -10,13 +10,11 @@ from os import path
 if len(sys.argv) > 1:
     data_dir = sys.argv[1]
 else:
-    data_dir = '/home/inky/Desktop/datasets/dtd/imgs_organized'
+    data_dir = '/home/inky/Desktop/datasets/dtd/images'
 
-train_data_dir = data_dir + '/train'
-val_data_dir = data_dir + '/val'
 num_classes = 47
-num_epochs = 50
-steps_per_epoch = 150
+num_epochs = 200
+steps_per_epoch = 500
 batch_size = 32
 
 train_datagen = ImageDataGenerator(
@@ -27,18 +25,8 @@ train_datagen = ImageDataGenerator(
     fill_mode='constant',
     rescale=1./255)
 
-val_datagen = ImageDataGenerator(
-    zoom_range=0.2,
-    fill_mode='constant',
-    rescale=1./255)
-
 train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(277, 277),
-        batch_size=batch_size)
-
-val_generator = val_datagen.flow_from_directory(
-        val_data_dir,
+        data_dir,
         target_size=(277, 277),
         batch_size=batch_size)
 
@@ -63,7 +51,7 @@ def get_train_data():
             train_features = np.concatenate([train_features, predictions])
             train_labels = np.concatenate([train_labels, labels])
         i += 1
-        if i == 150:
+        if i == steps_per_epoch:
             break
 
     np.save(open('train_features.resnet50.npy', 'w'), train_features)
@@ -71,40 +59,13 @@ def get_train_data():
 
     return train_features, train_labels
 
-def get_val_data():
-    if path.isfile('val_features.resnet50.npy') and path.isfile('val_labels.resnet50.npy'):
-        val_features = np.load(open('val_features.resnet50.npy'))
-        val_labels = np.load(open('val_labels.resnet50.npy'))
-        return val_features, val_labels
-
-    model = ResNet50(
-        include_top=False,
-        weights='imagenet',
-        input_shape=(277, 277, 3))
-
-    i = 0
-    for batch, labels in val_generator:
-        predictions = model.predict_on_batch(batch)
-        if i == 0:
-            val_features = predictions
-            val_labels = labels
-        else:
-            val_features = np.concatenate([val_features, predictions])
-            val_labels = np.concatenate([val_labels, labels])
-        i += 1
-        if i == 30:
-            break
-
-    np.save(open('val_features.resnet50.npy', 'w'), val_features)
-    np.save(open('val_labels.resnet50.npy', 'w'), val_labels)
-
-    return val_features, val_labels
-
 train_data, train_labels = get_train_data()
 
 model = Sequential()
 model.add(Flatten(input_shape=train_data.shape[1:]))
-model.add(Dense(2048, activation='relu'))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
@@ -114,8 +75,8 @@ model.compile(
     metrics=['accuracy'])
 
 model.fit(train_data, train_labels,
-          epochs=50,
+          epochs=num_epochs,
           batch_size=batch_size,
-          validation_data=get_val_data())
+          validation_split=0.15)
 
 model.save_weights('bottleneck_fc_model.resnet50.h5')
