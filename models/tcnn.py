044a58af9d8ddb2b213b0aa56971aaecea14439f
Jorillo <jorillo311@gmail.com>
2017-05-13 12:34:05 +0500

    +

--- models/tcnn.py
+++ models/tcnn.py
@@ -53,25 +53,28 @@ def TCNN2(input_shape=(277, 277, 3), classes=47):
     img_input = Input(shape=input_shape)
 
     # block 1
-    x = Conv2D(66, (3, 3), padding='same')(img_input)
+    x = Conv2D(64, (3, 3), padding='same')(img_input)
+    x = Conv2D(64, (3, 3), padding='same')(img_input)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     # block 2
-    x = Conv2D(121, (3, 3), padding='same')(x)
+    x = Conv2D(128, (3, 3), padding='same')(x)
+    x = Conv2D(128, (3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     # block 3
-    x = Conv2D(176, (3, 3), padding='same')(x)
+    x = Conv2D(256, (3, 3), padding='same')(x)
+    x = Conv2D(256, (3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
     # block 4
-    x = Conv2D(220, (3, 3), padding='same')(x)
+    x = Conv2D(512, (3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
--- train_tcnn.py
+++ train_tcnn.py
@@ -18,16 +18,16 @@ parser = OptionParser()
 
 parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
 parser.add_option("-v", "--val", dest="val_path", help="Path to validation data.")
-parser.add_option("-m", "--model", dest="model_name", help="Model name. Defaults to TCNN")
+parser.add_option("-m", "--model", dest="model_name", help="Model name. Defaults to TCNN", default="TCNN")
 parser.add_option("-s", "--suffix", dest="suffix",
 				help="Model will be saved with provided suffix, i.e. bottleneck_fc_model.[suffix].h5.")
 parser.add_option("--num-epochs", dest="num_epochs", help="Number of epochs. Defaults to 100", default=100)
 parser.add_option("--batch-size", dest="batch_size",
 				help="Batch size. Defaults to 16", default=16)
 parser.add_option("--steps-per-epoch", dest="steps_per_epoch",
-				help="Steps per epoch. Defaults to 100", default=100)
+				help="Steps per epoch. Defaults to 100", default=300)
 parser.add_option("--validation-steps", dest="validation_steps",
-				help="Validation steps. Defaults to 14", default=14)
+				help="Validation steps. Defaults to 14", default=60)
 parser.add_option("--target-size", dest="target_size",
 				help="Target size to resize images to. Defaults to 227", default=227)
 parser.add_option("--optimizer", dest="optimizer",
